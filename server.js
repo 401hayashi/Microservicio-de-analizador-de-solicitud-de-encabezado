d535889e6ffc488f520d3c61faa8c3596d1bc558
401hayashi <122819802+401hayashi@users.noreply.github.com>
2025-09-16 17:32:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+app.use(cors({ optionsSuccessStatus: 200 })); // habilitar CORS
+
+// Página inicial opcional
+app.get("/", (req, res) => {
+  res.send("Request Header Parser Microservice");
+});
+
+// Ruta principal del desafío
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  });
+});
+
+// Puerto dinámico
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor corriendo en puerto ${port}`);
+});
